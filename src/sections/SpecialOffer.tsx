b846c3a01ad603351ec8d7b9ce26728a5b31a816
ddimkou <ddimkou@outlook.com>
2024-04-10 14:19:51 +0300

    card styling changes

--- src/sections/SpecialOffer.tsx
+++ src/sections/SpecialOffer.tsx
@@ -13,21 +13,27 @@ const SpecialOffer = () => {
         {offers.map((offer: OfferProps, index: number) => (
           <div
             key={index}
-            className="w-80 h-96 border-2 border-black rounded-md shadow-xl bg-soft-warm-light
+            className="w-80 h-96 border-2 border-black rounded-md shadow-xl
+            hover:bg-soft-warm-light hover:shadow-2xl 
           flex flex-col items-center justify-between"
           >
-            <div className="w-full h-28 flex justify-center bg-white">
+            <div className="w-full  flex flex-col justify-center ">
               <img
                 src={offer.img}
                 alt={offer.title}
-                className="max-w-full object-contain"
+                className="max-w-full h-28 object-contain"
               />
+              <div className="p-4 font-bold text-lg text-center">
+                {offer.title}
+              </div>
             </div>
-            <div className="p-4 font-bold text-lg text-center">
-              {offer.title}
-            </div>
-            <div className="p-4 pb-10 text-sm font-bold text-gray-700 text-center">
-              {offer.subtitle}
+            <div className="flex flex-col justify-center">
+              <div className="p-4 pb-10 text-sm font-bold text-center ">
+                {offer.subtitle}
+              </div>
+              <button className="mb-2 p-2 w-2/4 rounded-md font-semibold text-white  bg-pale-red transition-colors duration-300 hover:bg-pale-pink hover:text-black mx-auto">
+                Read more
+              </button>
             </div>
           </div>
         ))}
